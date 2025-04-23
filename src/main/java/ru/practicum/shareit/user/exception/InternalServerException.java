4ec91e8f3355662d6ebb7f089789babd56f0c119
dt68 <destruct.you.down@gmail.com>
2025-04-23 12:40:25 +0300

    Commit #1 тест

--- src/main/java/ru/practicum/shareit/user/exception/InternalServerException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.user.exception;
-
-public class InternalServerException extends RuntimeException {
-    public InternalServerException(String message) {
-        super(message);
-    }
-}
