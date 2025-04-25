a9ecff7fc6be61bb1c6edb5f88649f0d49d6955f
dt68 <destruct.you.down@gmail.com>
2025-04-25 20:19:10 +0300

    Commit #5 добавил несколько тестов

--- server/src/test/java/ru/practicum/shareit/request/repository/ItemRequestRepositoryTest.java
+++ server/src/test/java/ru/practicum/shareit/request/repository/ItemRequestRepositoryTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import ru.practicum.shareit.request.model.ItemRequest;
